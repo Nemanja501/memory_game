fe675edd2ca17e8a7937c317f21321586040a34a
Nemanja501 <nemanjavulin5@gmail.com>
2023-11-11 23:30:12 +0100

    made the game functional

--- index.js
+++ index.js
@@ -1,48 +1,78 @@
-const numberOfClicks = 0;
+const grid = document.querySelector('.grid');
+const score = document.querySelector('#score');
+const cards = [];
+const imgSources = ['images/pixelart_1543145550507.png', 'images/pixelart_1543145569196.png', 'images/pixelart_1543145582966.png', 'images/pixelart_1543145606763.png', 'images/pixelart_1543145620562.png', 'images/pixelart_1543145673530.png', 'images/pixelart_1543145686904.png', 'images/pixelart_1543145702760.png', 'images/pixelart_1543145715155.png'];
 
-
-class Card {
-
-  constructor(color, number) {
-    this.color = color;
-    this.number = number;
-    this.#init();
+class Card{
+  constructor(imgSrc){
+    this.imgSrc = imgSrc;
+    this.init();
   }
 
-  #init() {
-    this.el = document.createElement("div");
-    this.el.style.background = this.color;
-    this.el.style.height = "200px";
-    this.el.style.width = "100px";
-    document.body.appendChild(this.el);
-
-    this.el.addEventListener('click', () => {
-        this.el.innerHTML = this.number;
-        numberOfClicks++;
-    })
+  init(){
+    this.el = document.createElement('img');
+    this.el.style.height = '200px';
+    this.el.style.width = '200px';
+    this.el.style.position = 'relative';
+    cards.push(this);
   }
 }
 
-const cardAmount = 12;
-const uniqueNumbers = cardAmount / 2;
-const numbers = [];
-for (let i = 0; i < uniqueNumbers; i++) {
-  let randomNumber = Math.floor(Math.random() * 99);
-  while (numbers.includes(randomNumber)) {
-    randomNumber = Math.floor(Math.random() * 99);
-  } 
-  numbers.push(randomNumber);
+function addCards(){
+  imgSources.forEach(image => {
+    const card = new Card(image);
+    card.el.setAttribute('src', 'images/c28fd55fd5dde7e576b53e1eabc585cf.jpg');
+  });
+  imgSources.forEach(image => {
+    const card = new Card(image);
+    card.el.setAttribute('src', 'images/c28fd55fd5dde7e576b53e1eabc585cf.jpg');
+  });
+  const shuffleCards = cards.sort(()=>Math.random() - 0.5);
+  shuffleCards.forEach(card => {
+    grid.appendChild(card.el);
+  });
 }
 
-let cards = [];
-for (let i = 0; i < uniqueNumbers; i++) {
+function playGame(){
+  let chosenCards = [];
+  let points = 0;
+  let func;
+  cards.forEach(card => {
+    card.el.addEventListener('click', func = ()=>{
+      card.el.setAttribute('src', card.imgSrc);
+      chosenCards.push(card);
 
-    const card = new Card("green", numbers[i]);
-    cards.push(card);
+      if(chosenCards.length == 2){
+        if(compareCards(chosenCards[0], chosenCards[1])){
+          setTimeout(()=>{
+            chosenCards[0].el.style.display = 'none';
+            chosenCards[1].el.style.display = 'none';
+            chosenCards[0].el.removeEventListener('click', func);
+            chosenCards[0].el.removeEventListener('click', func);
+            points++;
+            score.innerHTML = "Score: " + points;
+            chosenCards.pop();
+            chosenCards.pop();
+          }, 700);
+        }else{
+          setTimeout(()=>{
+            chosenCards[0].el.setAttribute('src', 'images/c28fd55fd5dde7e576b53e1eabc585cf.jpg');
+            chosenCards[1].el.setAttribute('src', 'images/c28fd55fd5dde7e576b53e1eabc585cf.jpg');
+            chosenCards.pop();
+            chosenCards.pop();
+          }, 700);
+      }
+    }
+    });
+  });
 }
-for (let i = 0; i < uniqueNumbers; i++) {
 
-    const card = new Card("green", numbers[i]);
-    cards.push(card);
+function compareCards(card1, card2){
+  if(card1.imgSrc == card2.imgSrc){
+    return true
+  }
+  return false;
 }
 
+addCards();
+playGame();
