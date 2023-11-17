2191a28a0e8e62ba8e9428f76bbcae2358682b01
Nemanja501 <nemanjavulin5@gmail.com>
2023-11-17 18:54:08 +0100

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const grid = document.querySelector('.grid');
 const score = document.querySelector('#score');
 const cards = [];
+let chosenCards = [];
+let points = 0;
 const imgSources = ['images/pixelart_1543145550507.png', 'images/pixelart_1543145569196.png', 'images/pixelart_1543145582966.png', 'images/pixelart_1543145606763.png', 'images/pixelart_1543145620562.png', 'images/pixelart_1543145673530.png', 'images/pixelart_1543145686904.png', 'images/pixelart_1543145702760.png', 'images/pixelart_1543145715155.png'];
 
 class Card{
@@ -33,33 +35,30 @@ function addCards(){
   });
 }
 
+
 function playGame(){
   let chosenCards = [];
   let points = 0;
-  let func;
   cards.forEach(card => {
-    card.el.addEventListener('click', func = ()=>{
-      card.el.setAttribute('src', card.imgSrc);
+    card.el.addEventListener('click', function func(){
+      flipCard(card);
       chosenCards.push(card);
-
       if(chosenCards.length == 2){
-        if(compareCards(chosenCards[0], chosenCards[1])){
+        let temp1 = chosenCards[0];
+        let temp2 = chosenCards[1];
+        chosenCards = [];
+        if(compareCards(temp1, temp2)){
+          temp1.el.removeEventListener('click', func);
+          temp2.el.removeEventListener('click', func);
           setTimeout(()=>{
-            chosenCards[0].el.style.display = 'none';
-            chosenCards[1].el.style.display = 'none';
-            chosenCards[0].el.removeEventListener('click', func);
-            chosenCards[0].el.removeEventListener('click', func);
+            correctPair(temp1, temp2);
             points++;
             score.innerHTML = "Score: " + points;
-            chosenCards.pop();
-            chosenCards.pop();
           }, 700);
         }else{
           setTimeout(()=>{
-            chosenCards[0].el.setAttribute('src', 'images/c28fd55fd5dde7e576b53e1eabc585cf.jpg');
-            chosenCards[1].el.setAttribute('src', 'images/c28fd55fd5dde7e576b53e1eabc585cf.jpg');
-            chosenCards.pop();
-            chosenCards.pop();
+            hideCard(temp1);
+            hideCard(temp2);
           }, 700);
       }
     }
@@ -67,11 +66,36 @@ function playGame(){
   });
 }
 
+
+function flipCard(selectedCard){
+  cards.forEach(card => {
+    if(selectedCard == card){
+      card.el.setAttribute('src', card.imgSrc);
+    }
+  });
+}
+
+function hideCard(selectedCard){
+  cards.forEach(card => {
+    if(selectedCard == card){
+      card.el.setAttribute('src', 'images/c28fd55fd5dde7e576b53e1eabc585cf.jpg');
+    }
+  });
+}
+
 function compareCards(card1, card2){
   if(card1.imgSrc == card2.imgSrc){
-    return true
+    return true;
   }
   return false;
+}
+
+function correctPair(card1, card2){
+  cards.forEach(card => {
+    if(card1 == card || card2 == card){
+      card.el.setAttribute('src', 'images/Solid_white.png');
+    }
+  });
 }
 
 addCards();
